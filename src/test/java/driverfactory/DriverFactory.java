94e5d33aba8e4e83025662baedc76245c422c87f
aditya2001 <aditya.200187@gmail.com>
2024-08-18 12:39:33 +0530

    Added configuration for cross browser testing

--- src/test/java/driverfactory/DriverFactory.java
+++ src/test/java/driverfactory/DriverFactory.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.safari.SafariDriver;
 
 
@@ -33,8 +34,10 @@ public class DriverFactory {
 			WebDriverManager.chromedriver().setup();
 			tlDriver.set(new ChromeDriver(options));
 		} else if (browser.equals("firefox")) {
+			FirefoxOptions options = new FirefoxOptions();
+			options.addArguments("-headless");
 			WebDriverManager.firefoxdriver().setup();
-			tlDriver.set(new FirefoxDriver());
+			tlDriver.set(new FirefoxDriver(options));
 		} else if (browser.equals("safari")) {
 			tlDriver.set(new SafariDriver());
 		} else {
